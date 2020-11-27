c0b1f7b0ab4861a2d8e76a6ece14103ec46db11f
Kristina <tina91kris11@gmail.com>
2020-11-27 14:21:45 +0100

    Added Footer component to the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // Imports from Components
 const { Switch, Route } = require("react-router-dom");
+const { default: Footer } = require("./Components/Content/Footer");
 const { default: Navbar } = require("./Components/Content/Nav");
 const { default: Dashboard } = require("./Components/Home/Dashboard");
 const { default: ProjectDetails } = require("./Components/Project/ProjectDetails");
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Dashboard} />
         <Route path="/project/:id" component={ProjectDetails} />
       </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Content/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return ( 
+        <footer className="footer-copyright">
+            <div className="footer-text">
+            &copy; 2020
+            </div>
+        </footer>
+     );
+}
+ 
+export default Footer;
