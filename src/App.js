c26c907408608c41ac0391ab1be8683b1c41acec
Kristina <tina91kris11@gmail.com>
2020-11-27 13:41:50 +0100

    Put some dummy data in ProjectDetails, added Route and passed props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 const { Switch, Route } = require("react-router-dom");
 const { default: Navbar } = require("./Components/Content/Nav");
 const { default: Dashboard } = require("./Components/Home/Dashboard");
+const { default: ProjectDetails } = require("./Components/Project/ProjectDetails");
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Switch>
-        <Route path="/" component={Dashboard} />
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/project/:id" component={ProjectDetails} />
       </Switch>
     </div>
   );
---
+++ src/Components/Project/ProjectDetails.js
@@ -0,0 +1,20 @@
+const ProjectDetails = (props) => {
+    console.log(props)
+    const id = props.match.params.id;
+    return ( 
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Project Title - {id}</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis asperiores soluta ut enim, laboriosam cum nobis vero dolorem eligendi reprehenderit aliquam illo, natus labore repudiandae ab adipisci consequuntur a officiis?</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>Posted by Kristina</div>
+                    <div>27.11.2020</div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default ProjectDetails;
