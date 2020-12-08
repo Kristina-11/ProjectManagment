82f4c8c01b64cd72412d38dd7839022e9e211431
Kristina <tina91kris11@gmail.com>
2020-12-08 13:08:23 +0100

    Connecting redux to firebase

--- src/index.js
+++ src/index.js
@@ -6,12 +6,25 @@ import '../node_modules/materialize-css/dist/js/materialize.js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import rootReducer from './Store/Reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// In order to connect firestore and firebase to redux we need to use store enhancers
+// Connecting everything with combining several store enhancers with compose
+
+const store = createStore(
+  rootReducer, 
+  compose(
+      applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+      reduxFirestore(fbConfig),
+      reactReduxFirebase(fbConfig)
+    )
+  );
 
 ReactDOM.render(
   <React.StrictMode>
