d8e6c7149404fe37168e3327cdcb512546aa5e34
Kristina <tina91kris11@gmail.com>
2020-12-21 14:23:28 +0100

    Styled links on a page.

--- src/Components/Home/Theory.js
+++ src/Components/Home/Theory.js
@@ -1,12 +1,14 @@
+const { NavLink } = require("react-router-dom")
+
 const Theory = () => {
     return (
         <div className="theory">
             <h2>Project Managment</h2>
             <p>
-                A project is a big responsibility, but you can make it easier to handle by planning it out in advance. This is done by <b>identifying</b> your project's <b>goals</b> and breaking them down into <b>actionable steps</b>. While staying aware of any budget or time constraints, you'll need to adjust your plans as the project takes shape. Then, by communicating with the project members, you'll manage the project to success.
+                A project is a big responsibility, but you can make it easier to handle by planning it out in advance. This is done by <span className='pop'>identifying</span> your project's <span className='pop'>goals</span> and breaking them down into <span className='pop'>actionable steps</span>. While staying aware of any budget or time constraints, you'll need to adjust your plans as the project takes shape. Then, by communicating with the project members, you'll manage the project to success.
                 <br/>
-                <i>Sign Up to start creating your projects. 
-                Or Log In to track your progress.</i>
+                <i><NavLink to="/signup" className='links'>Sign Up</NavLink> to start creating your projects. 
+                Or <NavLink to="/signin" className='links'>Log In</NavLink> to track your progress.</i>
             </p>
         </div>
       );
